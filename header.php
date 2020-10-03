4a6e3116d6e62dba51310a8c442184c65c715adb
Blasselle <thib.blasselle@gmail.com>
2020-10-03 16:12:10 +0200

    header + footer
    Ajout d'un header et d'un footer

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="header.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<header>
+    <h1>
+        CPourUnQuizz
+    </h1>
+    <a href="home.php"> Home </a>
+    <a href="login.php"> Log in </a>
+    <a href="register.php"> Register </a>
+    <a href="logout.php"> Log out </a>
+    <a href="quizz.php"> Quizz </a>
+    <a href="account.php"> Account </a>
+</header>
+<body>
+    
+</body>
+</html>
