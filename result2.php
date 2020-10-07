d3bbf282c6a7302756260d0891d048c7f3259440
mege <mathias.mege@isen.yncrea.fr>
2020-10-07 20:47:22 +0200

    ajout du quizz2 et résultats 2 + css

---
+++ result2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Résultats du quizz n°2</title>
+        <link rel="stylesheet" href="result2.css">
+    </head>
+    <body>
+        <div>
+            <p>
+                <img src="logo.png" alt=""><br>
+                <h1>Résultats du quizz n°2</h1>
+            </p>
+        </div>
+        <div>
+            <p>
+                <h1>Votre score est de 4/4, vous êtes un fin connaisseur</h1>
+            </p>
+            <p>
+                <img src="gg.gif" alt="">
+            </p>
+        </div>
+        <div>
+            <form action="quizz2.php">
+                <p>
+                    <button type="submit">Réessayer</button>
+                </p>
+            </form>
+            <form action="home.php">
+                <p>
+                    <button type="submit">Retour à l'accueil</button>
+                </p>
+            </form> 
+        </div>
+    </body>
+</html>
