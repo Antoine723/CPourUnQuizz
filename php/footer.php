c4a5f2c3ae8f4c55fa82fa6b42ad3fc1ba12861e
Nicolas <nicolas.defoort@isen.yncrea.fr>
2020-10-23 18:01:43 +0200

    nouveau footer + img

--- php/footer.php
+++ php/footer.php
@@ -3,7 +3,8 @@
     <link rel="stylesheet" href="../css/footer.css">
 
     <footer>
-        <img class = "display" src = "../images/logo_footer.jpg">
-        <p> Y a quelqu'un qui fait notre quizz là ?!</p>
+        <img class="img_footer1"  src="../images/yeux.png">
+        <p class="text_footer"> Y a quelqu'un qui fait notre quizz là ?!</p>
+        <img class="img_footer2" src="../images/Photo_palma.PNG" alt="" width="5cm">
     </footer>
 </html>
