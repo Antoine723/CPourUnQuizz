2ff73dc6dacc5e4655b41812b2b1f501b3b0971e
Erwan Wattelier <erwan.wattelier@isen.yncrea.fr>
2020-11-04 12:09:11 +0100

    Correction affichage image
    Modif chemin d'accès à l'image dans header et footer
    + Suppr dans quizz.css

--- php/footer.php
+++ php/footer.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="../css/footer.css">
 
     <footer>
-        <img class = "display" src = "../images/logo_footer.jpg">
+        <img class = "display" src = "images/logo_footer.jpg">
         <p> Y a quelqu'un qui fait notre quizz là ?!</p>
     </footer>
 </html>
