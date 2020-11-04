bcffe3097f4a82f26fcc6e070ea18e26fbd7f808
Erwan Wattelier <erwan.wattelier@isen.yncrea.fr>
2020-11-04 16:09:19 +0100

    Suppression page resultat du header

--- php/header.php
+++ php/header.php
@@ -24,7 +24,6 @@
                     <?php }
                         else{?>
                         <a class="navigate" href="index.php?page=homequizz">Quizz</a>
-                        <a class="navigate" href="index.php?page=result">Résultat</a>
                         <a class="navigate" href="index.php?page=leaderboard">Classement</a>
                         <a class="navigate" href="index.php?user=account">Profil</a>
                         <a class="navigate" href="index.php?user=logout" id="logout"><img src="https://img.icons8.com/ios/50/000000/exit.png" alt="Log out"/></a>
