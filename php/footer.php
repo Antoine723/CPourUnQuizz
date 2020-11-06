c5b41052adf5cb1faef4c55554516ec782bd6f75
Antoine <antoinea723@gmail.com>
2020-11-06 17:33:18 +0100

    Modif structure index + CSS

--- php/footer.php
+++ php/footer.php
@@ -1,22 +1,20 @@
 <!DOCTYPE html>
-<html>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/footer.css">
 
-    <hr>
-    <footer>
-        <div class="first">
-            <p class="textone"> 
-            ©CPourUnQuizz<br>                                 
-            Contact us with the mail : <a href="">CPourUnQuizz@outlook.com</a>
-            </p>
-        </div>
-        <div class="second">
-            <p class="texttwo">
-            <b>Adresse<b><br>
-            20 rue des églantiers<br>
-            59800 Lille
-            </p> 
-        </div>
-    </footer>
-</html>
+<div class="first">
+    <p class="textone"> 
+        ©CPourUnQuizz<br>                                 
+        Contact us with the mail : <a href="">CPourUnQuizz@outlook.com</a>
+    </p>
+</div>
+<div class="second">
+    <p class="texttwo">
+        <b>Adresse<b><br>
+        20 rue des églantiers<br>
+        59800 Lille
+    </p> 
+</div>
+<div class="third">
+    <p> <!--Pour que l'image soit alignée avec le reste du footer-->
+        <img src="images/footerpic.png" height="80">
+    </p>
+</div>
