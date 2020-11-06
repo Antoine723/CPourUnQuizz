028bbabfb6dbc815d781fe7661e07d6aed9f4d85
mege <mathias.mege@isen.yncrea.fr>
2020-11-06 15:59:00 +0100

    footer php+css changes

--- php/footer.php
+++ php/footer.php
@@ -1,10 +1,28 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
+    <meta charset="UTF-8">
     <link rel="stylesheet" href="../css/footer.css">
 
+    <hr>
     <footer>
-        <p class="text_footer"> Y a quelqu'un qui fait notre quizz là ?!</p>
-        <img class="img_footer2" src="images/Photo_palma.PNG" alt="" width="5cm">
-        <img class="img_footer1"  src="images/yeux.png">
+        <div class="first">
+            <p class="textone"> 
+            ©CPourUnQuizz<br>                                 
+            Contact us with the mail : <a href="">CPourUnQuizz@outlook.com</a>
+            </p>
+        </div>
+        <div class="second">
+            <p class="texttwo">
+            <b>Adresse<b><br>
+            20 rue des églantiers<br>
+            59800 Lille
+            </p> 
+        </div> 
+        <div class="third">
+            <p class="textthree">
+            Find us on social networks 
+            </p> 
+            <img src="fblogo.png" height="50"> 
+        </div> 
     </footer>
 </html>
