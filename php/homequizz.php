7ca329adae0e3c802ff7cd09663252650d89c471
Nicolas <nicolas.defoort@isen.yncrea.fr>
2020-11-18 17:45:31 +0100

    update homequizz.php ajout des boutons result

--- php/homequizz.php
+++ php/homequizz.php
@@ -23,3 +23,39 @@
         </p>
     </div>
 </div>
+<!-- on crée des boutons si le quizzid correspond a celui qui a été fait par l'utilisateur(vérifier argument fonction)  -->
+<div class="box"> 
+    <div class="element">
+        <p><?php
+        
+            if (!empty(getAllScoresByIdPlayer($_SESSION["user_id"],$_GET["quizz_id"]==1))){?>
+    
+                <a href="index.php?page=quizz&id=1" class="button">Résultat quizz Chansons</a><?php
+
+            }
+            if (!empty(getAllScoresByIdPlayer($_SESSION["user_id"],$_GET["quizz_id"]==5))){?>
+                <a href="index.php?page=quizz&id=5" class="button">Résultat quizz Pub</a><?php
+            }?>
+        </p>
+    </div>
+    <div class="element">
+        <p><?php
+            if (!empty(getAllScoresByIdPlayer($_SESSION["user_id"],$_GET["quizz_id"]==4))){?>
+            <a href="index.php?page=quizz&id=4" class="button">Résultat quizz Parodie émission TV</a><?php
+            }
+            if (!empty(getAllScoresByIdPlayer($_SESSION["user_id"],$_GET["quizz_id"]==2))){?>
+            <a href="index.php?page=quizz&id=2" class="button">Résultat quizz Amateur</a><?php
+            }?>
+        </p>
+    </div>
+    
+    <div class="element">
+        <p><?php
+            if (!empty(getAllScoresByIdPlayer($_SESSION["user_id"],$_GET["quizz_id"]==3))){?>
+            <a href="index.php?page=quizz&id=3" class="button">Résultat quizz Expert</a><?php
+            }?>
+        </p>
+    </div>
+</div>
+
+
